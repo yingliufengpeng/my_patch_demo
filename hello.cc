4de3aa7ea83acaa2b8ce1f2ab1ec29c618052d91
yingliufengpeng <yingliufengpeng@gmail.com>
2025-09-03 08:30:14 +0000

    add  add function

---
+++ hello.cc
@@ -0,0 +1,10 @@
+
+#include <iostream>
+#include "hello.h"
+
+
+
+int main() {
+   auto m = add(4, 4);
+   return 0;
+}
---
+++ hello.h
@@ -0,0 +1,13 @@
+
+//
+// Created by peng on 15/5/2025.
+//
+
+#ifndef BAZEL_PROJECT1_MY_HEADER_H
+#define BAZEL_PROJECT1_MY_HEADER_H
+
+int add(int a, int  b);
+
+#endif //BAZEL_PROJECT1_MY_HEADER_H
+
+
